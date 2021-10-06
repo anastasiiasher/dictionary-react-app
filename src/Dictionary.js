5426912e009033a59fce95e7d7e9e9b991c9f892
anastasiiasher <nastyasher@me.com>
2021-10-06 22:31:33 +0200

    API inject

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
@@ -7,6 +7,12 @@ export default function Dictionary() {
     event.preventDefault();
     alert(`${keyword}`);
   }
+  function handleResponse(response) {
+    console.log(response[0]);
+  }
+  let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+  axios.get(apiUrl).then(handleResponse);
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
