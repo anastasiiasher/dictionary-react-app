473f59f7969b094a36bebce95386def891b1e5cc
anastasiiasher <nastyasher@me.com>
2021-10-06 23:23:24 +0200

    API functions

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import axios from "axios";
+import Results from "./Results.js";
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
+  let [results, setResults] = useState(null);
 
   function search(event) {
     event.preventDefault();
-    alert(`${keyword}`);
   }
   function handleResponse(response) {
-    console.log(response[0]);
+    console.log(response.data[0]);
+    setResults(response.data[0]);
   }
   let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
   axios.get(apiUrl).then(handleResponse);
@@ -29,6 +31,7 @@ export default function Dictionary() {
           <input type="submit" placeholder="Search" />
         </form>
       </header>
+      <Results results={results} />
     </div>
   );
 }
